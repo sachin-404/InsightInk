d6db11488722cfaff106ac3154d128c80893628a
Sachin Kant <sachinkant404@gmail.com>
2023-09-10 23:55:53 +0530

    configured staticfiles

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('froala_editor/',include('froala_editor.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+urlpatterns += staticfiles_urlpatterns()
