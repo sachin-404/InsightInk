7255b217483814f9df0f85996ea2eaeeb6603082
Sachin Kant <sachinkant404@gmail.com>
2023-09-11 01:29:44 +0530

    integrated templates

--- Blog/home/urls.py
+++ Blog/home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name="home"),
+    path('login', views.login_view, name="login_view"),
+    path('signup', views.signup_view, name="signup_view"),
 ]
