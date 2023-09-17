84f93b4932c62736fe8ff420fa19798a09ce0c4a
Sachin Kant <sachinkant404@gmail.com>
2023-09-18 00:04:20 +0530

    registration implementation and some minor ui changes

--- Blog/public/static/js/main.js
+++ Blog/public/static/js/main.js
@@ -32,3 +32,36 @@ function login(){
 
 
 }
+
+function register(){
+    var username = document.getElementById("loginUsername").value
+    var password = document.getElementById("loginPassword").value
+    csrf= document.getElementById("csrf").value
+
+    if(username == '' || password == ''){
+        alert("username and password can not be blank")
+    }
+
+    var data = {
+        'username': username,
+        'password': password
+    }
+
+    fetch('/api/signup/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrf
+        },
+        body: JSON.stringify(data)
+    }).then(result => result.json())
+    .then(response => {
+        console.log(response)
+        if (response.status == 200){
+            window.location.href = '/login'
+            alert("Registration Succesfull. Please Login to continue")
+        } else {
+            alert(response.message)
+        }
+    })
+}
